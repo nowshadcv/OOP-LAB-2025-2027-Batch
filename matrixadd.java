2cfab07c1f8acdafd6e2ad3fdc16df238f24d595
hasnasinu <hasnasinu55@gmail.com>
2026-02-03 16:19:30 +0530

    Add files via upload

--- matrixadd.java
+++ matrixadd.java
@@ -1,50 +1,50 @@
-package pgm3;
+package matrix;
 import java.util.Scanner;
+
 public class matrixadd {
 	public static void main(String[] args) {
 		int p,q,m,n;
-		Scanner s=new Scanner (System.in);
-		System.out.print("enter 0f rows in matrix A:");
+		Scanner s=new Scanner(System.in);
+		System.out.print("ente rnumber of rows in matrix a:");
 		p=s.nextInt();
-		System.out.print("enter 0f columnss in matrix A:");
+		System.out.print("enter number of columns in matrix a:");
 		q=s.nextInt();
-		System.out.print("enter 0f rows in matrix B:");
+		System.out.print("enter number of rows in matrix b:");
 		m=s.nextInt();
-		System.out.print("enter 0f columnss in matrix B:");
+		System.out.print("enter number of columns in matrix b:");
 		n=s.nextInt();
-		
-		if (p==m&&q==n) {
-			int a[][] = new int[p][q];
-			int b[][] = new int[m][n];
-			System.out.println("enter 0f rows in matrix A:");
-			for (int i = 0 ; i < p ; i++)
-				for(int j =0 ; j < q ; j++)
-					a[i][j] = s.nextInt();
-			System.out.println("enter the elements  0f matrix B:");
-			for (int i = 0 ; i < m ; i++)
-				for(int j =0 ; j < n ; j++)
-					b[i][j] = s.nextInt();
-			System.out.println("matrix A:");
-				for (int i = 0 ; i < p ; i++) {
-					for(int j =0 ; j < q ; j++)
-						System.out.println(a[i][j]+ " ");
-					System.out.println();}
-			System.out.println("matrix B:");
-			for (int i = 0 ; i < m ; i++) {
-				for(int j =0 ; j < n ; j++)
-					System.out.println(b[i][j]+ " ");
-				System.out.println() ;}
-			System.out.println("the sum matrix:"); 
-			for (int i = 0 ; i < m ; i++) {
-				for(int j =0 ; j < n ; j++)
-					System.out.println((a[i][j]+b[i][j])+ " ");
+		if(p==m&&q==n) {
+			int a[][]=new int[p][q];
+			int b[][]=new int[m][n];
+			System.out.print("enter the elements of matrix a:");
+			for(int i=0;i<p;i++)
+				for(int j=0;j<q;j++)
+					a[i][j]=s.nextInt();
+			System.out.println("enter the elements of matrix B:");
+			for(int i=0;i<m;i++)
+				for(int j=0;j<n;j++)
+					b[i][j]=s.nextInt();
+			System.out.println("matrix a:");
+			for(int i=0;i<p;i++) {
+				for(int j=0;j<q;j++)
+					System.out.print(a[i][j]+" ");
+			    System.out.println();}
+			System.out.println("matrix b:");
+			for(int i=0;i<m;i++) {
+				for(int j=0;j<n;j++)
+					System.out.print(b[i][j]+" ");
+			    System.out.println();}
+			System.out.println("the sum matrix:");
+			for(int i=0;i<m;i++) {
+				for(int j=0;j<n;j++)
+					System.out.print((a[i][j]+b[i][j])+ " ");
 				System.out.println();}
-		}
-		else {
-			System.out.println("these matrix cannot be added..");}
-		
-		}
-		
-		
-		
+			    
 	}
+	else {
+		System.out.println("these matrix cannot be added");
+	}
+	
+	}
+
+}
