ec87a1571d5dc5ca3f0152040b7056fd4ea1c954
krishnapriyakp732-cmyk <krishnapriyakp732@gmail.com>
2026-02-10 15:48:02 +0530

    Add files via upload

---
+++ 31-krishnapriyakp/product_class/product.java
@@ -0,0 +1,39 @@
+package OOPLAB;
+
+public class product {
+	public class cars {
+
+	}
+	String pname,pcode;
+	int price;
+	public product() {}
+	public product(String pname,String pcode,int price) {
+		this.pname=pname;
+		this.pcode=pcode;
+		this.price=price;}
+	public void setpname(String pname) {
+		this.pname=pname;}
+	public String getpcode() {
+		return pcode;}
+	public String getpname() {
+		return pname;}
+	public void setpcode(String pcode) {
+		this.pcode=pcode;}
+	public int getprice() {
+		return price;}
+	public void setprice(int price) {
+		this.price=price;}
+	public void display() {
+		System.out.println("pcode:"+ this.pcode);
+		System.out.println("pname:"+ this.pname);
+		System.out.println("price:"+ this.price+"\n");}}
+
+	
+	
+	
+		
+	
+
+	
+
+
