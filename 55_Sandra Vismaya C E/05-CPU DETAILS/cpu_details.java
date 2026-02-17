16651ce435b7e8ac0a74afed7bb2cae031c8eca0
sandravce <sandravismaya@gmail.com>
2026-02-17 16:22:01 +0530

    Update cpu_details.java

--- 55_Sandra Vismaya C E/05-CPU DETAILS/cpu_details.java
+++ 55_Sandra Vismaya C E/05-CPU DETAILS/cpu_details.java
@@ -1,35 +1,40 @@
 package OOPLAB;
 
-class CPU {   
-    double price;
+import java.util.Scanner;
 
-    class Processor {
-        double cores;
-        String manufacturer;
-
-        double getCache() {
-            return 4.3;
-        }
-    }
-
-    static class RAM {
-        double memory;
-        String manufacturer;
-
-        double getClockSpeed() {
-            return 5.5;
-        }
-    }
+public class cpu_details
+{
+	double price;
+	class Processor
+	{
+		double cores;
+		String manufactures;
+		double getcache(double c)
+		{
+			return c;
+		}
+	}
+	static class RAM
+	{
+		double memory;
+		String manufactures;
+		double getClockSpeed(double s)
+		{
+			return s;
+		}
+	}
+	public static void main(String[] args)
+	{
+		Scanner sc=new Scanner(System.in);
+		cpu_details cpu=new cpu_details();
+		cpu_details.Processor processor= cpu.new Processor();
+		cpu_details.RAM ram=new cpu_details.RAM();
+		System.out.println("Enter the cache :");
+		double c=sc.nextDouble();
+		System.out.println("Enter the Clock Speed :");
+		double s=sc.nextDouble();
+		System.out.println("processor cache= "+processor.getcache(c));
+		System.out.println("RAM clock speed ="+ram.getClockSpeed(s));
+		sc.close();
+	}
 }
-
-public class cpu_details {   
-    public static void main(String args[]) {
-        CPU cpu = new CPU();
-        CPU.Processor processor = cpu.new Processor();
-        CPU.RAM ram = new CPU.RAM();
-
-        System.out.println("Processor Cache = " + processor.getCache());
-        System.out.println("RAM Clock speed = " + ram.getClockSpeed());
-    }
-}
-
