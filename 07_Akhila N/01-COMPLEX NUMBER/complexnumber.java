04e2a2395051676d134feef1dbd611976081830b
akhila-nair2005 <167734842+akhila-nair2005@users.noreply.github.com>
2026-02-17 16:08:25 +0530

    Add files via upload

---
+++ 07_Akhila N/01-COMPLEX NUMBER/complexnumber.java
@@ -0,0 +1,26 @@
+package OOPLAB;
+
+public class complexnumber {
+	double real, img;
+	
+	complexnumber(double r, double i)
+	{
+		real=r;
+		img=i;
+	}
+	public static complexnumber sum(complexnumber c1, complexnumber c2)
+	{
+		complexnumber temp=new complexnumber(0,0);
+		temp.real=c1.real+c2.real;
+		temp.img=c1.img+c2.img;
+		return temp;
+	}
+	public static void main(String args[])
+	{
+		complexnumber c1=new complexnumber(5.5,4);
+		complexnumber c2=new complexnumber(1.2,3.5);
+		complexnumber temp=sum(c1,c2);
+		System.out.printf("Sum is:"+temp.real+"+"+temp.img+"i");
+	}
+
+}
