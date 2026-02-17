c55e8febd926119ebbe13128daf6935f9f46812d
ahamedshadi1234-alt <ahamedshadi1234@gmail.com>
2026-02-17 16:20:45 +0530

    Add files via upload

---
+++ 03_Ahamed Shadi/06-STRING SORT/stringsort.java
@@ -0,0 +1,36 @@
+package program6;
+import java.util.Arrays;
+import java.util.Scanner;
+public class stringsort {
+public static void main(String[]args) {
+	int count=0;
+	String tmp;
+	Scanner scan = new Scanner(System.in);
+	System.out.println("enter the number of string to sort");
+	count=scan.nextInt();
+	String strlist[]=new String[count];
+	Scanner scan1=new Scanner(System.in);
+	System.out.println("enter your strings");
+	for(int i = 0;i<count;i++)
+		strlist[i]=scan1.nextLine();
+	System.out.println("Choose 1 or n2 from the menu below:");
+	System.out.println("1.IN-Built sort");
+	System.out.println("2: user-Defined sort");
+	int choice;
+	choice=scan.nextInt();
+	switch(choice) {
+	case 1:Arrays.sort(strlist);
+		System.out.println(Arrays.toString(strlist));
+		break;
+	case 2: for(int i=0;i<count-1;i++)
+		for(int j=i+1;j<strlist.length;j++)
+			if(strlist[i].compareTo(strlist[j])>0) {
+				tmp=strlist[i];
+				strlist[i]=strlist[j];
+				strlist[j]=tmp;}
+	System.out.println(Arrays.toString(strlist));
+	break;}
+			}
+	
+}
+
