67bced979bb2191a5a0fd8adcf9687fa16929f91
Mufeenathesni <mufeenathesni7@gmail.com>
2026-02-17 16:23:31 +0530

    Add files via upload

---
+++ 41_Mufeena Thesni P/01-COMPLEXNUMBERS/complexnumber.java
@@ -0,0 +1,39 @@
+
+package OOPLAB;
+
+import java.util.Scanner;
+
+public class complexnumber {
+	double real,img;
+	complexnumber(double r, double i){
+		real=r;
+		img=i;
+		
+	}
+	public static complexnumber sum(complexnumber c1, complexnumber c2) {
+		complexnumber temp= new complexnumber(0,0);
+		temp.real= c1.real+c2.real;
+		temp.img=c1.img+c2.img;
+		return temp;
+	}
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter real part of first number: ");
+        double r1 = sc.nextDouble();
+        System.out.print("Enter imaginary part of first number: ");
+        double i1 = sc.nextDouble();
+
+        System.out.print("Enter real part of second number: ");
+        double r2 = sc.nextDouble();
+        System.out.print("Enter imaginary part of second number: ");
+        double i2 = sc.nextDouble();
+		complexnumber c1= new complexnumber(r1,i1);
+		complexnumber c2= new complexnumber(r2,i2);
+		complexnumber temp= sum(c1,c2);
+		System.out.printf("Sum is:"+temp.real+"+"+temp.img+"i");
+
+	}
+
+
+}
