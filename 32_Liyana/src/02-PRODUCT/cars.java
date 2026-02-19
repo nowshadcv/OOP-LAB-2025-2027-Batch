246170cab79fc546081f2c61306f7094fd9dd79f
ImLiyana <160301024+ImLiyana@users.noreply.github.com>
2026-02-19 13:40:11 +0530

    Add files via upload

---
+++ 32_Liyana/src/02-PRODUCT/cars.java
@@ -0,0 +1,29 @@
+package complex.java;
+
+
+
+public class cars{
+	public static void main(String[] args) {
+		
+		product p1 = new product();
+		p1.pcode = "car123";
+		p1.pname = "Benz";
+		p1.price = 10000;
+		System.out.println("Displaying p1: ");
+		p1.display();
+		
+		product p2 = new product("Jaguar","car123",25000);
+		System.out.println("Displaying p2: ");
+		p2.display();
+		
+		product p3 = new product("Maruthi","car800",50000);
+		System.out.println("Displaying p2: ");
+		p3.display();
+		
+		//product p = p3.getprice()<(p1.price<p2.price?p1.price:p2.price)?p3:= (p1.price<p2.price?p1:p2);
+		product p = p1.price<p2.price?p1:p2;
+		System.out.println("\ndisplaying product with lowest price:");
+		p.display();
+	
+	}
+}
