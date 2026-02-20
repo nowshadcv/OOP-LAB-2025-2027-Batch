d9cbeaa43a4e8339f2390f684ab37d9907899fd4
muhsina <mmuhsi226@gmail.com>
2026-02-20 11:31:51 +0530

    Add files via upload

---
+++ 19_Muhsina/CPUDETAILS/cpudetails.java
@@ -0,0 +1,34 @@
+package OOPLAB;
+
+class CPU {   
+    double price;
+
+    class Processor {
+        double cores;
+        String manufacturer;
+
+        double getCache() {
+            return 4.3;
+        }
+    }
+
+    static class RAM {
+        double memory;
+        String manufacturer;
+
+        double getClockSpeed() {
+            return 5.5;
+        }
+    }
+}
+
+public class cpu\details {   
+    public static void main(String args[]) {
+        CPU cpu = new CPU();
+        CPU.Processor processor = cpu.new Processor();
+        CPU.RAM ram = new CPU.RAM();
+
+        System.out.println("Processor Cache = " + processor.getCache());
+        System.out.println("RAM Clock speed = " + ram.getClockSpeed());
+    }
+}
