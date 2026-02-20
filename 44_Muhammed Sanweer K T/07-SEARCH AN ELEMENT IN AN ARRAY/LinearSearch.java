67775da7eafa72a982c7a505e5c674bc9cdee796
sanweer-sr <sanweersr@gmail.com>
2026-02-20 11:47:47 +0530

    asdasd

---
+++ 44_Muhammed Sanweer K T/07-SEARCH AN ELEMENT IN AN ARRAY/LinearSearch.java
@@ -0,0 +1,27 @@
+package MODULE_1;
+
+
+import java.util.Scanner;
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		int c,n,search,array[];
+		Scanner in =new Scanner(System.in);
+		System.out.println("Enter Number of Elements ");
+		n=in.nextInt();
+		array=new int[n];
+		System.out.println("Enter Those "+n+" elements");
+		for(c=0;c<n;c++)
+			array[c]=in.nextInt();
+		System.out.println("Enter Value You Want to Find : ");
+		search=in.nextInt();
+		for(c=0;c<n;c++)
+			if(array[c] == search) {
+				System.out.println(search + " is present in the location :" +(c+1));
+				break;
+			}
+			if(c==n)
+				System.out.println(search+" is not present in  the array " );
+	}
+
+}
