320a35063681841a1d3d84c8f8b6e67e06761199
sanweer-sr <sanweersr@gmail.com>
2026-02-27 11:31:52 +0530

    cpudetails update

--- 44_Muhammed Sanweer K T/05-CPU DETAILS/CPU.java
+++ 44_Muhammed Sanweer K T/05-CPU DETAILS/CPU.java
@@ -1,42 +1,38 @@
-
 import java.util.Scanner;
 
 public class CPU {
 
     double price;
 
-   
     class Processor {
         int cores;
         String manufacturer;
-
-        double getCache() {
-            return 4.3;
-        }
+        double cache;
 
         void getProcessorData(Scanner sc) {
             System.out.print("Enter number of cores: ");
             cores = sc.nextInt();
-            sc.nextLine();  
+            sc.nextLine();
 
             System.out.print("Enter processor manufacturer: ");
             manufacturer = sc.nextLine();
+
+            System.out.print("Enter processor cache (MB): ");
+            cache = sc.nextDouble();
+            sc.nextLine();
         }
 
         void displayProcessorData() {
             System.out.println("Processor Cores: " + cores);
             System.out.println("Processor Manufacturer: " + manufacturer);
-            System.out.println("Processor Cache: " + getCache() + " MB");
+            System.out.println("Processor Cache: " + cache + " MB");
         }
     }
 
     static class RAM {
         int memory;
         String manufacturer;
-
-        double getClockSpeed() {
-            return 5.5;
-        }
+        double clock;
 
         void getRAMData(Scanner sc) {
             System.out.print("Enter RAM memory (GB): ");
@@ -45,12 +41,16 @@ public class CPU {
 
             System.out.print("Enter RAM manufacturer: ");
             manufacturer = sc.nextLine();
+
+            System.out.print("Enter RAM clock speed (GHz): ");
+            clock = sc.nextDouble();
+            sc.nextLine();
         }
 
         void displayRAMData() {
             System.out.println("RAM Memory: " + memory + " GB");
             System.out.println("RAM Manufacturer: " + manufacturer);
-            System.out.println("RAM Clock Speed: " + getClockSpeed() + " GHz");
+            System.out.println("RAM Clock Speed: " + clock + " GHz");
         }
     }
 
@@ -62,13 +62,11 @@ public class CPU {
         CPU.Processor processor = cpu.new Processor();
         CPU.RAM ram = new CPU.RAM();
 
-   
         processor.getProcessorData(sc);
         ram.getRAMData(sc);
 
         System.out.println("\n----- CPU DETAILS -----");
 
-       
         processor.displayProcessorData();
         ram.displayRAMData();
 
