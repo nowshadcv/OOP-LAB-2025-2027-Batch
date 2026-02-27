d0eafe9525324b1e9fc75a6d826447ef06228bba
MUHAMMED ALTHAF O K <althff10@gmail.com>
2026-02-27 11:22:30 +0530

    Update SymmericMatrix.java

--- 42_Muhammed Althaf O K/04_MATRIX SYMMETRIC/SymmericMatrix.java
+++ 42_Muhammed Althaf O K/04_MATRIX SYMMETRIC/SymmericMatrix.java
@@ -1,4 +1,4 @@
-package matrix;
+package OOPLAB;
 import java.util.Scanner;
 public class SymmericMatrix {
 	public static void main(String[]args) {
@@ -37,3 +37,4 @@ public class SymmericMatrix {
 	}
     		   
 }
+
