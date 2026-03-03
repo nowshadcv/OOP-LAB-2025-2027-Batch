ec3a4243fc2d4c6cfa41acf729091f7b020c5b6d
Abishek <abhi.k87007@gmail.com>
2026-03-03 14:54:08 +0530

    Add files via upload

---
+++ 02_Abishek/08-STRINGMANIPULATION/stringmanipulation.java
@@ -0,0 +1,53 @@
+package MCA_S2_02;
+
+public class stringmanipulation {
+	public static void main ( String[] args ) {
+		char arrSample[] = {'R','O','S','E'};
+		
+		String strSample1=new String(arrSample);
+		System.out.println(strSample1+"\t");
+		System.out.println("Length of "+strSample1+" is : "+strSample1.length());
+		
+		byte ascii[]= {65,66,67,68,69,70};
+		String strSample2=new String(ascii);
+		System.out.println(strSample2+"\t");
+		
+		String strSample3=strSample1.concat(strSample2);
+		System.out.println(strSample3+"\t");
+		
+		String strSample4=strSample1+strSample2;
+		System.out.println(strSample4+"\t");
+		System.out.println("3rd char of "+strSample4+"is: " +strSample4.charAt(2));
+		
+		char buf[]=new char[3];
+		strSample4.getChars(2, 5, buf, 0);
+		System.out.println("Extracted chars : "+strSample4+""+new String(buf));
+		
+		String strSample="RockStar";
+		System.out.println(strSample.compareTo("ROCKSTAR")+"\t");
+		System.out.println(strSample.compareToIgnoreCase("ROCKSTAR")+"\t");
+		System.out.println(strSample.equals("ROCKSTAR")+"\t");
+		System.out.println(strSample.equalsIgnoreCase("ROCKSTAR")+"\t");
+		System.out.println("RockStar starts with 'Ro': "+strSample.startsWith("Ro"));
+		System.out.println("RockStar ends with 'Star': "+strSample.endsWith("Star"));
+		System.out.println("RockStar contains 'tar': "+strSample.contains("tar"));
+		System.out.println("Index of 't' in RockStar: "+strSample.indexOf("t"));
+		System.out.println("Index of 'star' in RockStar: "+strSample.indexOf("Star"));
+		System.out.println("All caps - RockStar: "+strSample.toUpperCase());
+		System.out.println("All small - RockStar: "+strSample.toLowerCase());
+		System.out.println("Replace star with et: "+strSample.replace("Star","et"));
+		
+		String dVal="3.456";
+		String iVal="6";
+		System.out.println("Double value of dVal is: "+Double.valueOf(dVal));
+		System.out.println("Integer value of iVal is: "+Integer.valueOf(iVal));
+		double a=Double.valueOf(dVal);
+		int b=Integer.valueOf(iVal);
+		System.out.println(a+b);
+	}
+}
+
+	
+
+
+
