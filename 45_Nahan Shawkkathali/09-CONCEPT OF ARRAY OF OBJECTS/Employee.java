e3943401002b2ecac6d66dc040a724e05869cadc
nahan-shawkkathali <nahanstory@gmail.com>
2026-03-06 11:18:23 +0530

    Add files via upload

--- 45_Nahan Shawkkathali/09-CONCEPT OF ARRAY OF OBJECTS/Employee.java
+++ 45_Nahan Shawkkathali/09-CONCEPT OF ARRAY OF OBJECTS/Employee.java
@@ -1,5 +1,7 @@
 package OOPLAB;
 
+import java.util.Scanner;
+
 public class Employee {
 	int eNo,eSalary;
 	String eName;
@@ -15,4 +17,52 @@ public void showData()
 	System.out.print("EmpId="+eNo+" "+"Name="+eName+" "+"salary="+eSalary);
 	System.out.println();
 }
+
+	public static void main(String [] args)
+	{
+		System.out.println("Enter number of employees:");
+		Scanner sc=new Scanner (System.in);
+		int n=sc.nextInt();
+		System.out.println("Enter employee details one by one");
+		Employee employees[]=new Employee[n];
+		Scanner scemp=new Scanner(System.in);
+		int eid,esal;
+		String enam;
+		for(int i=0;i<n;i++)
+		{
+			System.out.println("enter"+i+"employee details");
+			System.out.println("enter employee id(integer):");
+			eid=scemp.nextInt();
+			System.out.println("Enter employee name(String):");
+			String nam=scemp.next();
+			enam=new String(nam);
+			System.out.println("enter employee salary(integer");
+			esal=scemp.nextInt();
+			Employee emp=new Employee(eid,esal,enam);
+			employees[i]=emp;
+		}
+		System.out.println("employees are:\n");
+		for(Employee y:employees)
+			y.showData();
+		System.out.println("enter employee number to search:");
+		int semp=sc.nextInt();
+		boolean found=false;
+		for(Employee e:employees)
+		{
+			if(semp==e.eNo) {
+				found=true;
+				System.out.println("employee found");
+				e.showData();
+				break;
+			
+			}
+		}
+	   if(!found)
+	   {
+		   System.out.println("Employee not found....");
+	   }
+	   sc.close();
+	}
+	
+
 }
