8447cb96d2bd6d9730bde64c342c9f24130c8fb1
MUHAMMED ALTHAF O K <althff10@gmail.com>
2026-03-10 13:44:44 +0530

    Add files via upload

---
+++ 42_Muhammed Althaf O K/10_METHOD OVERLOADING/OverLoad.java
@@ -0,0 +1,38 @@
+package OOPLAB;
+
+import java.util.Scanner;
+public class OverLoad {
+	void area(float x) {
+		System.out.println("the area of square "+ Math.pow(x, 2)+" sq units");
+	}
+	void area (float x, float y) {
+		float rect = x * y; 
+		System.out.println("the area of the rectangle is " +rect+  " sq units");
+	}
+	void area(double x) {
+		double circ = 3.14*x*x;
+		System.out.println("the area of the circle is " +circ+ " sq units");
+	}
+	public static void main(String args[]) {
+		OverLoad ob = new OverLoad();
+		float a,b,c;
+		double d;
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter The Side of Square : ");
+		a=s.nextFloat();
+		ob.area(a) ;
+		
+		System.out.print("Enter The Length of Rectangle : ");
+		b=s.nextFloat();
+		System.out.print("Enter The Breadth of Rectangle : ");
+		c=s.nextFloat();
+		ob.area(b,c);
+		
+		System.out.print("Enter The Radius of Circle : ");
+		d=s.nextDouble();
+		ob.area(d);
+		
+		s.close();
+	}
+}
+	
