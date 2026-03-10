f4024a2a1d89cc63c5b4741c48990ad85ac2e3db
Mohammed safvan. N <safvanmohammedn@gmail.com>
2026-03-10 13:50:58 +0530

    Add files via upload

---
+++ 40_Mohammed Safvan N/10-METHOD OVERLOADING/OverLoad.java
@@ -0,0 +1,22 @@
+package OOPLAB;
+
+class OverLoadDemo{
+	void area(float x) {
+		System.out.println("The area of the square is " + Math.pow(x, 2)+ " sq units");
+	}
+	void area(float x,float y) {
+		System.out.println("the are of the rectangle is " + x*y +" sq units");
+	}
+	void area(double x) {
+		double z= 3.14 * x * x;
+		System.out.println("the are of the circle is " + z +" sq units");
+	}
+}
+public class OverLoad {
+	public static void main(String[] args) {
+		OverLoadDemo ob = new OverLoadDemo();
+		ob.area(5);
+		ob.area(11, 12);
+		ob.area(2.5);
+	}
+}
