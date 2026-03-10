34426eed33cdc4c0c2ab5160f14b5b7f98d85888
AparnaVNambissan <aparnanambisaan@gmail.com>
2026-03-10 14:38:40 +0530

    Add files via upload

---
+++ 11_Aparna/10-METHOD OVERLOADING/OverLoadDemo.java
@@ -0,0 +1,27 @@
+package OOPLAB;
+
+public class OverLoadDemo {
+	void area (float x) {
+		System.out.println("the area of the square is:"+Math.pow(x, 2)+"square");
+	
+	}
+	void area(float x,float y) {
+		System.out.println("The area of the rectangle is "+x*y+"sq.units"); }
+	void area(double x) {
+		double z=3.14*x*x;
+		System.out.println("The area of the circle is "+z+"sq.units");
+		
+	}
+	}
+	class OverLoad {
+		public static void main(String[] args) {
+			OverLoadDemo ob=new OverLoadDemo();
+			ob.area(5);
+			ob.area(11,12);
+			ob.area(2.5);
+			
+		}
+	}
+	
+
+
