1a6b583db4867e6436963b21366ba5ec0cc31cf2
akhila-nair2005 <167734842+akhila-nair2005@users.noreply.github.com>
2026-03-18 13:46:49 +0530

    Add files via upload

---
+++ 07_Akhila N/12-INTERFACE/IFaceDemo.java
@@ -0,0 +1,91 @@
+package OOPLAB;
+import java.util.Scanner;
+
+	interface AP{
+		void input();
+		void area();
+		void perimeter();
+	}
+	class circle implements AP{
+		int r=0;
+		double pi=3.14,area=0,perimeter=0;
+		public void input() {
+			Scanner c=new Scanner(System.in);
+			r=c.nextInt();
+		}
+		public void area() {
+			area=pi*r*r;
+			System.out.println("Area of the circle:"+area);
+		}
+		public void perimeter() {
+			perimeter = 2*pi * r;
+			System.out.println("perimeter of the circle:"+perimeter);
+		}
+	}
+	class rectangle implements AP{
+		int l=0,b=0;
+		double area,perimeter;
+		public void input() {
+			Scanner r=new Scanner(System.in);
+			l=r.nextInt();
+			b=r.nextInt();
+		}
+		public void area() {
+			area=l*b;
+			System.out.println("Area of the rectangle:"+area);
+		}
+		public void perimeter() {
+			perimeter = 2*(l+b);
+			System.out.println("perimeter of the rectangle:"+perimeter);
+		}
+	}
+	public class IFaceDemo{
+		public static void main(String[] args) {
+			int ch;
+			circle c=new circle();
+			rectangle r=new rectangle();
+			Scanner sc=new Scanner(System.in);
+			System.out.println("1. Area of the circle:");
+			System.out.println("2. Area of the rectangle:");
+			System.out.println("3. perimeter of the circle:");
+			System.out.println("4. perimeter of the rectangle:");
+			System.out.println("5. Program Termination:");
+			
+			lp:while(true) {
+				System.out.println("Make your choice:");
+				ch=sc.nextInt();
+				switch(ch) {
+				case 1:
+					System.out.println("Enter the radius of the circle:");
+					c.input();
+					c.area();
+					break;
+				case 2:
+					System.out.println("Enter the length and breadth of the rectangle :");
+					r.input();
+					r.area();
+					break;
+				case 3:
+					System.out.println("Enter the radius of the circle:");
+					c.input();
+					c.perimeter();
+					break;
+				case 4:
+					System.out.println("Enter the length and breadth of the rectangle:");
+					r.input();
+					r.perimeter();
+					break;
+				case 5:
+					System.out.println("Exiting...");
+					break lp;
+				default:
+					System.out.println("Invalid choice:");
+				}
+
+			}
+			
+		}
+		
+	}
+
+
