eb4a783ebf815de8b9eb8a5403f1a6da29b418e0
Arathi1104 <arathik2711@gmail.com>
2026-03-31 14:58:18 +0530

    Add files via upload

---
+++ 12_Arathi K/15-INHERITANCE PUBLISHER/inheritancebook.java
@@ -0,0 +1,81 @@
+package OOPLAB;
+
+import java.util.Scanner;
+
+class Publisher { 
+    String publisher; 
+    Publisher(String publi){
+        this.publisher = publi ;
+    }
+}
+
+class Book { 
+    String name; 
+    Publisher publisher; 
+    
+    Book () { } 
+    
+    public Book (String name, Publisher publisher ) { 
+        this.name = name ; 
+        this.publisher = publisher ;
+    }
+} 
+
+class Literature extends Book { 
+    String Lit_type="Literature"; 
+    
+    Literature (String name, Publisher publisher ) { 
+        super (name, publisher);
+    } 
+    
+    void display () { 
+        System.out.println("Name: " + super.name);
+        System.out.println("Type: " + this.Lit_type);
+        System.out.println("Publisher: " + this.publisher.publisher);
+    }
+} 
+
+class Fiction extends Book { 
+    String Lit_type="Fiction"; 
+    
+    Fiction (String name, Publisher publisher ) { 
+        super (name, publisher);
+    } 
+    
+    void display () {
+        System.out.println("Name: " + super.name); 
+        System.out.println("Type: " + this.Lit_type); 
+        System.out.println("Publisher: " + this.publisher.publisher);
+    }
+} 
+
+public class inheritancebook { 
+    public static void main(String[] args){ 
+        
+        Scanner sc = new Scanner(System.in);
+
+        
+        System.out.print("Enter Literature Book Name: ");
+        String lname = sc.nextLine();
+        System.out.print("Enter Literature Publisher: ");
+        String lpub = sc.nextLine();
+
+        Publisher lp = new Publisher(lpub);
+        Literature l = new Literature(lname, lp);
+
+    
+        System.out.print("Enter Fiction Book Name: ");
+        String fname = sc.nextLine();
+        System.out.print("Enter Fiction Publisher: ");
+        String fpub = sc.nextLine();
+
+        Publisher fp = new Publisher(fpub);
+        Fiction f = new Fiction(fname, fp);
+
+        System.out.println("\n--- Book Details ---");
+        l.display();
+        f.display();
+
+        sc.close();
+    }
+}
