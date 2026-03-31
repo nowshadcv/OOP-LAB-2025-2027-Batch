4c5f4227fc959a11bff115a3eee3321c06b421b0
najaa-22 <naja59613@gmail.com>
2026-03-31 15:01:37 +0530

    Add files via upload

---
+++ 46_Najamujeeb T/16-INTERFACE STUDENTS AND SPORTS/sportstudentresult.java
@@ -0,0 +1,40 @@
+package OOPLAB;
+
+import java.util.Scanner;
+
+interface student{
+	void displayscore();
+}
+interface sports{
+	void displaysportsscore();
+}
+class result implements student,sports{
+	int academicScore;
+    int sportsScore;
+    
+    result(int ac, int sp) {
+        academicScore = ac;
+        sportsScore = sp;
+    }
+	public void displayscore() {
+		System.out.println("Academic score is:"+academicScore);
+		}
+	
+	public void displaysportsscore() {
+		System.out.println("Sports score is:"+sportsScore);
+		}
+
+	}
+public class sportstudentresult {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the student academic score: ");
+        int ac = sc.nextInt();
+        System.out.println("Enter the student sports score: ");
+        int sp = sc.nextInt();
+		result r=new result(ac, sp);
+		r.displayscore();
+		r.displaysportsscore();
+		sc.close();
+	}
+}
