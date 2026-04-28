70d85a6e4ba66a54dc51d59ade20e56aabfaf550
gaaya3tp-design <gaaya3tp@gmail.com>
2026-04-28 14:39:50 +0530

    Add files via upload

---
+++ 21_Gayathri T P/18-PACKAGE-ARITHMETIC OPERATIONS/Arithmetic/Divide.java
@@ -0,0 +1,11 @@
+package Arithmetic;
+
+public class Divide implements ArithmeticOperations {
+    public double operate(double a, double b) {
+        if (b == 0) {
+            System.out.println("Division by zero is not allowed.");
+            return 0;
+        }
+        return a / b;
+    }
+}
